01a796580d157f8810e84fe3ce3580b74a9a848b
sergio.ibar <sergio.ibar@lambdaloopers.com>
2022-02-21 16:12:50 +0100

    Problem 5

--- p5.py
+++ p5.py
@@ -3,3 +3,21 @@
 
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 """
+
+result = 0
+step = 0
+
+def is_divisible_by_all(num):
+   for i in range(1,20):
+      if num % i != 0:
+         return False
+   return True
+
+#The result has to be divisible by all prime numbers below 20, 
+#this product will be the iteration step 
+
+while(result == 0):
+   step += 19*17*13*11*7*5*3*2
+   if is_divisible_by_all(step):
+      result = step
+      print(step)
